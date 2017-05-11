355afc217bdecd7357528d5d7954c043b4f43db0
Peter Altosaar <peter.altosaar@gmail.com>
2017-05-11 16:50:33 -0400

    PA/FG - Move credentials into non-repo file

---
+++ login.js
@@ -0,0 +1,3 @@
+var exports = module.exports = {};
+exports.username = "hackathon";
+exports.password = "!ntelliware*Dev_Bu!lD";
--- query.js
+++ query.js
@@ -1,30 +1,27 @@
 "use-strict";
 
-const endpoint = "https://api.pcf-system.intware.com";
-const username = "";
-const password = "";
+var credentials = require('login');
 
+const endpoint = "https://api.pcf-system.intware.com";
 const CloudController = new (require("cf-nodejs-client")).CloudController(endpoint);
 const UsersUAA = new (require("cf-nodejs-client")).UsersUAA;
 const Apps = new (require("cf-nodejs-client")).Apps(endpoint);
 const Events = new (require("cf-nodejs-client")).Events(endpoint);
 
-var accessToken;
-
 var exports = module.exports = {};
 
 exports.getCFEvents = function() {
 
-    CloudController.getInfo().then((result) = > {
+    CloudController.getInfo().then(function(result) {
         UsersUAA.setEndPoint(result.authorization_endpoint);
-        return UsersUAA.login(username, password);
-    }).then((result) = > {
+        return UsersUAA.login(credentials.username, credentials.password);
+    }).then(function(result) {
         Events.setToken(result);
         bearerToken = result.access_token;
         return Events.getEvents({q: "type:app.crash"});
-    }).then((result) = > {
+    }).then(function(result) {
         console.log(result);
-    }).catch((reason) = > {
+    }).catch(function(reason) {
         console.error("Error: " + reason);
     });
-}
+};
